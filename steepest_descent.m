9dd37d2e64e7e4344f6c0a90107fa2fb13c9012d
Mathias <Mathias@Mathias_Dator.lan>
2019-09-09 14:24:18 +0200

    More logical code

--- steepest_descent.m
+++ steepest_descent.m
@@ -1,11 +1,5 @@
 function [x_min, J_min] = steepest_descent(x0, func, func_gradient, N, beta)
-
-    % Calculate gradient
-    g = func_gradient(x0);
-    
-    % Start guess for alfa
-    alfa0 = 0.01*norm(x0)/norm(g);
-    
+        
     % Counter
     t = 0;
     
@@ -19,18 +13,21 @@ function [x_min, J_min] = steepest_descent(x0, func, func_gradient, N, beta)
     % Store t, J and alfa values in vectors
     t_vector = 0:1:N-1;
     J_vector = NaN(1, N);
-    alfa_vector = NaN(1, N);
+    alfa_vector = NaN(1, N); 
     
-    while norm(g) > thresh
+   % 1. Calculate J0 for x0
+   J0 = func(x0);
 
-        % 1. Calculate J0 for x0
-        J0 = func(x0);
-
-        % 2. Calculate the gradient in the point x0
-        g = func_gradient(x0);
-
+   % 2. Calculate the gradient in the point x0
+   g0 = func_gradient(x0);
+   
+   % Start guess for alfa
+   alfa0 = 0.01*norm(x0)/norm(g0);
+    
+    while norm(g0) > thresh
+        
         % 3. Calculate x1
-        x1 = x0 - alfa0*g;
+        x1 = x0 - alfa0*g0;
 
         % 4. Calculate the new function value J1
         J1 = func(x1);
@@ -42,15 +39,19 @@ function [x_min, J_min] = steepest_descent(x0, func, func_gradient, N, beta)
             
             % Store values
             alfa_vector(t+1) = alfa0;
-            J_vector(t+1) = J1;
+            J_vector(t+1) = J0;
             
-            %Update alfa
+            % Update alfa
             alfa0 = alfa1;
+            
+            % Update gradient
+            g1 = func_gradient(x1);
             
             % Update indecies
             x0 = x1;
             t = t + 1;
-            
+            J0 = J1;
+            g0 = g1;
         else
             
             % 5b. If the new value is larger, reduce step size
@@ -58,16 +59,9 @@ function [x_min, J_min] = steepest_descent(x0, func, func_gradient, N, beta)
             % Reduce alfa
             alfa1 = (1 - beta) * alfa0;
 
-            % Store values
-            alfa_vector(t+1) = alfa0;
-            J_vector(t+1) = J1;
-            
-            % Calculate new J1
-            J1 = func(x1);
-            
             % Update alfa
             alfa0 = alfa1;
-  
+            
         end
        
         % Terminate if max iterations have been reached
